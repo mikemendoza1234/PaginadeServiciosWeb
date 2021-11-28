6ae9e0b15cb646b39e47ee39cba32585af3ff29f
ArturoCA <arturo.cardona107@gmail.com>
2021-11-28 11:05:09 -0600

    Se agregaron las tablas para la carga de la base de datos

---
+++ DB/tablas.sql
@@ -0,0 +1,53 @@
+CREATE TABLE usuario{
+    usuario_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    usuario VARCHAR(50) NOT NULL,
+    contrasenia VARCHAR(20),
+    telefono INT(11),
+    codigo VARCHAR(50)
+
+}
+
+CREATE TABLE servicios{
+    servicio_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    clave VARCHAR(50) NOT NULL,
+    precio FLOAT(6,2),
+    duracion_hrs INT(11)
+}
+
+CREATE TABLE pedidos(
+    pedidos_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT(11) NOT NULL,
+    tecnico_id INT(11) NOT NULL,
+    estatus CHAR(1) NOT NULL,
+    folio CHAR(10) NOT NULL,
+    fecha DATE NOT NULL,
+    fecha_inicio_gral DATE NOT NULL,
+    fecha_fin_gral DATE NOT NULL,
+    horas_necesarias INT(11) NOT NULL,
+    horas_trabajadas INT(11),
+    importe FLOAT(15,2),
+    notas VARCHAR(400),
+    FOREIGN KEY(usuario_id) REFERENCES usuario(usuario_id)
+    FOREIGN KEY(tecnico_id) REFERENCES tecnico(tecnico_id)
+
+)
+
+CREATE TABLE partidas_pedido{
+    partidas_pedido_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    pedidos_id INT(11) NOT NULL,
+    servicio_id INT(11) NOT NULL,
+    fecha_inicio DATE  NOT NULL,
+    fecha_fin DATE  NOT NULL,
+    estatus CHAR(1) NOT NULL,
+    FOREIGN KEY(pedidos_id) REFERENCES pedidos(pedidos_id)  ON DELETE CASCADE
+    FOREIGN KEY(servicio_id) REFERENCES servicios(servicio_id)
+}
+
+CREATE TABLE tecnico{
+    tecnico_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    numero_empleado VARCHAR(50),
+    contrasenia VARCHAR(20)
+}
